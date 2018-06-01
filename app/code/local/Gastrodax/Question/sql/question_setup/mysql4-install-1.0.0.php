076ee83fb3c20ee6b7a952f4b48a0be3a4ef457b
Froxlor Customer <magento@dev.gastro.lan>
2018-06-01 10:34:09 +0200

    change mysql-install and config.xml

--- app/code/local/Gastrodax/Question/sql/question_setup/mysql4-install-1.0.0.php
+++ app/code/local/Gastrodax/Question/sql/question_setup/mysql4-install-1.0.0.php
@@ -5,15 +5,16 @@ $installer = $this;
 $installer->startSetup();
 
 $installer->run("
-DROP TABLE IF EXISTS {$this->getTable('gd_question')};
+
+-- DROP TABLE IF EXISTS {$this->getTable('gd_question')};
 CREATE TABLE {$this->getTable('gd_question')} (
   `question_id` int(11) unsigned NOT NULL auto_increment,
   `customer_id` int(11) NULL,
   `customer_name` varchar(200) NOT NULL default '',
   `customer_email` varchar(200) NOT NULL default '',
   `sku` varchar(100) NOT NULL,
-  `comment` text NOT NULL default '',
   `dsgvo` smallint(6) NOT NULL default '0',
+  `comment` text NOT NULL default '',
   `created_time` datetime NOT NULL default CURRENT_TIMESTAMP,
   `status` varchar(10) NOT NULL default 'OPEN',
   PRIMARY KEY (`question_id`)
