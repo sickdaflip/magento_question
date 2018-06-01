c4af138a1e566de95133a5458cb40e5e208d5b12
Froxlor Customer <magento@dev.gastro.lan>
2018-06-01 10:43:53 +0200

    change mysql-install and config.xml

--- app/code/local/Gastrodax/Question/Block/Adminhtml/Question.php
+++ app/code/local/Gastrodax/Question/Block/Adminhtml/Question.php
@@ -7,7 +7,7 @@ class Gastrodax_Question_Block_Adminhtml_Question extends Mage_Adminhtml_Block_W
         $this->_blockGroup = 'question';
         $this->_headerText = Mage::helper('question')->__('question');
         $this->setTemplate('widget/grid/container.phtml');  // use for disable add new button
-        //$this->_addButtonLabel = Mage::helper('inquiry')->__('Add Item');
+        //$this->_addButtonLabel = Mage::helper('question')->__('Add Item');
         //parent::__construct(); //comment for not display Add new Button
     }
 
